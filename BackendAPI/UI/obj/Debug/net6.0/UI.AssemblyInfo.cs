47187817eaf0162570c7218f9a6f127e2306b3a9
Hemant Kumar <Hemant1096@outlook.com>
2023-11-19 21:56:12 +0530

    update

--- BackendAPI/UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
+++ BackendAPI/UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4bd5b4e52005008f12974e209460d6799cc5d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
